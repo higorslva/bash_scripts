72d9f39ff5b0128902275a795ab6689f4ae31831
Higor Silva <higors88@gmail.com>
2022-06-20 00:09:53 -0300

    Add support do remove lock from dpkg

--- chrome-installer.sh
+++ chrome-installer.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #github.com/higorslva
+
+#Removendo eventuais travas do dpkg
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+
+
 touch -> chrome-installer.log
 mkdir /home/$USER/chrome-tmp
 cd /home/$USER/chrome-tmp
-sudo apt-get update
+sudo apt update
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -> chrome-installer.log
 cd
