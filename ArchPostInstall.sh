b1913826963246931e24b83a8c706f2c10302c8f
Higor Silva <48228780+higorslva@users.noreply.github.com>
2022-11-27 18:14:43 -0300

    Removido os pacotes 'code' e 'atom'
    Já que o Atom ('atom') não é mais suportado e foi removido dos mirrors do ArchLinux, e mudei para o binário oficial do VS Code do AUR (pacote 'visual-studio-code' no lugar de 'code), ambos foram removidos do script.

--- ArchPostInstall.sh
+++ ArchPostInstall.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 ## Atualizando o repositório e um último upgrade ##
-pacman -Syyu
+sudo pacman -Syyu
 
 ## Mudança para o diretório de instalação do yay ##
 cd ~/Downloads/ || exit
 mkdir yay-git
 cd yay-git || exit
 ## Instalação do yay para o AUR ##
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd || exit
 
 # ---------------------------------------------------------------------- #
 # ----------------------------- EXECUÇÃO ----------------------------- #
 
 ## Download e instalaçao de programas do repositório oficial ##
-pacman -S libreoffice-still libreoffice-still-pt-br virtualbox flameshot atom bat bashtop code codeblocks docker geogebra gimp git mysql-workbench noto-fonts-emoji jdk-openjdk openssh postgreesql kleopatra gnome-keyring qbittorrent telegram-desktop unrar unzip virtualbox vlc zsh networkmanager --needed
+sudo pacman -S libreoffice-still libreoffice-still-pt-br virtualbox postgresql flameshot bat bashtop codeblocks docker geogebra gimp git mysql-workbench noto-fonts-emoji jdk-openjdk openssh kleopatra gnome-keyring qbittorrent telegram-desktop unrar unzip virtualbox vlc zsh networkmanager --needed
 
 ## Download e instalação de programas do AUR ##
-yay -S adw-gtk3-git chrome-gnome-shell-git debtap eclipse-jee hunspell-pt-br makedeb ncurses5-compat-libs otf-monacob-git ttf-monaco tuxguitar --needed
+yay -S adw-gtk3-git chrome-gnome-shell-git debtap eclipse-jee hunspell-pt-br makedeb visual-studio-code-bin ncurses5-compat-libs otf-monacob-git ttf-monaco tuxguitar --needed
 
 #Habilitando serviços do systemd
 systemctl enable NetworkManager
