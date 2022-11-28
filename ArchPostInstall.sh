8a88c54ec0477ed1b1d25831f7ec64151e113552
Higor Silva <higors88@gmail.com>
2022-11-27 21:33:08 -0300

    Adicionado pacotes nodejs e npm

--- ArchPostInstall.sh
+++ ArchPostInstall.sh
@@ -15,7 +15,7 @@ cd || exit
 # ----------------------------- EXECUÇÃO ----------------------------- #
 
 ## Download e instalaçao de programas do repositório oficial ##
-sudo pacman -S libreoffice-still libreoffice-still-pt-br virtualbox postgresql flameshot bat bashtop codeblocks docker geogebra gimp git mysql-workbench noto-fonts-emoji jdk-openjdk openssh kleopatra gnome-keyring qbittorrent telegram-desktop unrar unzip virtualbox vlc zsh networkmanager --needed
+sudo pacman -S nodejs npm libreoffice-still libreoffice-still-pt-br virtualbox postgresql flameshot bat bashtop codeblocks docker geogebra gimp git mysql-workbench noto-fonts-emoji jdk-openjdk openssh kleopatra gnome-keyring qbittorrent telegram-desktop unrar unzip virtualbox vlc zsh networkmanager --needed
 
 ## Download e instalação de programas do AUR ##
 yay -S adw-gtk3-git chrome-gnome-shell-git debtap eclipse-jee hunspell-pt-br makedeb visual-studio-code-bin ncurses5-compat-libs otf-monacob-git ttf-monaco tuxguitar --needed
